4b985cfcaa5f4f07201264b1a5b259fa0fa724d3
Lily Kiziriya <107429746+lk20918n@users.noreply.github.com>
2023-01-03 15:57:13 -0500

    started day02 problem

---
+++ day02/solution.py
@@ -0,0 +1,15 @@
+count = 0
+buf = 0
+
+dict = {'AX': 1+3, "AY": 2+6, "AZ": 3+0,
+        'BX': 1+0, "BY": 2+3, "BZ": 3+6,
+        'CX': 1+6, "CY": 2+0, "CZ": 3+3, }
+
+
+with open("test.txt") as f:
+    for line in f:
+        buf = line.replace(" ", "")
+        buf = line.replace("\n", "")
+
+
+# print(count)
