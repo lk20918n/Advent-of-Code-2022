61a03bc20d0f6ec6543ed96964d4c92f0c73c546
Lily Kiziriya <107429746+lk20918n@users.noreply.github.com>
2023-01-03 14:13:58 -0500

    solved day1 advent challenge

---
+++ Day1/solution_p1.py
@@ -0,0 +1,16 @@
+max = 0
+cur = 0
+
+with open("input.txt") as f:
+    for line in f:
+        if (line == "\n"):
+            max = cur if (cur > max) else max
+            # print("max is now: " + str(max))
+            cur = 0
+        else:
+            cur += int(line)
+            # print("cur is now: " + str(cur))
+
+print(max)
+
+# Space: O(1), Time O(n)
---
+++ Day1/solution_p2.py
@@ -0,0 +1,16 @@
+list = []
+sum = 0
+
+with open("input.txt") as f:
+    for line in f:
+        if (line == "\n"):
+            list.append(sum)
+            sum = 0
+        else:
+            sum += int(line)
+
+list.sort(reverse=True)
+# print(list)
+print(list[0]+list[1]+list[2])
+
+# Space: O(n), Time: O(n.logn)
